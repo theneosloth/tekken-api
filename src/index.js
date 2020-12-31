c07abaaa209355982e533bd9697582ccf13653d7
Stefan Kuznetsov <skuznetsov2@my.bcit.ca>
2020-12-31 00:06:55 -0800

    Adjusted the port

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { getCharData, getMoveData } = require('./get_char_data');
 
 const app = express();
-const port = 8080;
+const port = process.env.TEKKEN_API_PORT || 3000;
 
 app.use(express.json());
 
